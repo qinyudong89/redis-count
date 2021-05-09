0dbe5e9bde8c17bb4cfef536277565c050c9b371
morgan.qin <morgan.qin@watsons.com.cn>
2021-05-09 15:45:08 +0800

    Ridis实现计数功能

---
+++ src/main/java/com/example/rediscount/RedisCountApplication.java
@@ -0,0 +1,13 @@
+package com.example.rediscount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCountApplication.class, args);
+    }
+
+}
